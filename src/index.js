483b245b50a83cf20967244b033c0fefdc36c11a
KietLNA <kietlna1997>
2021-03-28 23:01:52 +0700

    [INTERNAL] Adding boostrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+var path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars  = require('express-handlebars');
 const port = 3000;
 const app = express();
-var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'))
 app.engine('hbs', handlebars({
   extname:'.hbs'
